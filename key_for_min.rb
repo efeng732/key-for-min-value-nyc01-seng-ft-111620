a033dfc7740e49b8621ed1e8f1ab9195796e0486
Erwin Feng <efeng732@gmail.com>
2020-11-04 20:55:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,11 @@
 
 def key_for_min_value(name_hash)
   min_num = 1000 
+  min_key = ""
   name_hash.each do |key, value|
     if min_num > value 
-      min_num = value 
+       min_num = value
+       min_key = key 
   end
 end 
 end 
